f79d62ad970c6673f657646fcb9bd232b1760fdb
Glutamat42 <markus.heck13396@gmx.de>
2023-07-07 16:35:56 +0200

    fix delete folder does not exist (on first run)

--- opt/adler/setup.sh
+++ opt/adler/setup.sh
@@ -8,7 +8,7 @@ else
         first_run=true
 fi
 
-rm -r /bitnami/moodle/adler_setup  # cleanup first
+rm -r -f /bitnami/moodle/adler_setup  # cleanup first
 cp -r /opt/adler/moodle/adler_setup /bitnami/moodle/
 chown -R daemon /bitnami/moodle/adler_setup
 su daemon -s /bin/sh -c "php /bitnami/moodle/adler_setup/setup.php --first_run=$first_run ${USER_NAME:+--user_name=$USER_NAME} ${USER_PASSWORD:+--user_password=$USER_PASSWORD} ${USER_FIRST_NAME:+--user_first_name=$USER_FIRST_NAME} ${USER_LAST_NAME:+--user_last_name=$USER_LAST_NAME} ${USER_EMAIL:+--user_email=$USER_EMAIL} ${USER_ROLE:+--user_role=$USER_ROLE} ${PLUGIN_VERSION:+--plugin_version=$PLUGIN_VERSION}"
